f640626230e653249259d09e359ec775aa6d8f48
nicolinabl <nicolina.backlin@telia.com>
2025-11-18 20:13:00 +0100

    Setting up basic structure + testing component

---
+++ src/Components/Test.jsx
@@ -0,0 +1,7 @@
+export const TestComponent = () => {
+  return (
+    <>
+      <h1>This is a test component</h1>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.jsx'
+import { TestComponent } from './Components/Test.jsx'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/*App^^is a component*/}
+    <TestComponent />
   </StrictMode>,
 )
