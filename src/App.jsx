9eb2e396f3c6a9bfeb228cd869bd6b9c76b6ac94
nicolinabl <nicolina.backlin@telia.com>
2025-11-27 10:57:41 +0100

    global style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './Components/layout/Header'
 import { MainSection } from './Components/layout/MainSection'
 import { Footer } from './Components/layout/Footer'
+import GlobalStyle from './globalStyles'
 
 export const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <MainSection />
       <Footer />
